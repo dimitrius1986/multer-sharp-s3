904508e43977f7f6980202bffc5d094517b8b534
Dimitrius Ionov <dimitrius.ionov@monsanto.com>
2019-04-29 17:25:57 -0700

    Adding PassThrough for array upload

--- src/main.ts
+++ src/main.ts
@@ -108,6 +108,7 @@ export class S3Storage implements StorageEngine {
     file: EFile,
     cb: (error?: any, info?: Info) => void
   ) {
+    console.log('_uploadProcess')
     const { opts, sharpOpts } = this
     let { stream, mimetype } = file
     const {
